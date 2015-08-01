77aeed5959ffed30716aa6565bcc49dcbd33af04
Sven Nilsen <bvssvni@gmail.com>
2015-08-01 21:22:39 +0200

    Added custom format for extracting information
    Closes https://github.com/PistonDevelopers/eco/issues/3

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,17 @@ extern crate piston_meta;
 use piston_meta::*;
 
 fn main() {
-    
+
+}
+
+#[cfg(test)]
+mod tests {
+    use piston_meta::*;
+
+    #[test]
+    fn extract() {
+        let data = load_syntax_data("assets/extract/syntax.txt",
+            "assets/extract/test.txt");
+        json::print(&data);
+    }
 }
