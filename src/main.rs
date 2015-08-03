8da86b09d619fd98a4632988b5f9f3b9782aac92
Sven Nilsen <bvssvni@gmail.com>
2015-08-03 18:34:27 +0200

    Added custom format for versions and dependencies
    Closes https://github.com/PistonDevelopers/eco/issues/10

--- src/main.rs
+++ src/main.rs
@@ -22,4 +22,17 @@ mod tests {
             "assets/extract/test.txt");
         // json::print(&_data);
     }
+
+    #[test]
+    fn dependencies_is_json() {
+        let _ = load_syntax_data("assets/json/syntax.txt",
+            "assets/dependencies/test.txt");
+    }
+
+    #[test]
+    fn dependencies() {
+        let _data = load_syntax_data("assets/dependencies/syntax.txt",
+            "assets/dependencies/test.txt");
+        // json::print(&_data);
+    }
 }
