04f73cbcf09851eca7a26cbeecb56456ca18033b
Sven Nilsen <bvssvni@gmail.com>
2015-08-09 20:47:39 +0200

    Added `load_text_file_from_url`
    See https://github.com/PistonDevelopers/eco/issues/5

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 //! A tool for reasoning about breaking changes in Rust ecosystems
 
 extern crate piston_meta;
+extern crate hyper;
 
 use std::rc::Rc;
 
@@ -22,12 +23,39 @@ pub struct Dependency {
     pub version: String,
 }
 
+/// Loads a text file from url.
+pub fn load_text_file_from_url(url: &str) -> Result<String, String> {
+    use hyper::client::Client;
+    use hyper::{Url};
+    use hyper::status::StatusCode;
+    use std::io::Read;
+
+    let url_address = try!(Url::parse(url)
+        .map_err(|e| format!("Error parsing url: {}", e)));
+    let client = Client::new();
+    let request = client.get(url_address);
+    let mut response = try!(request.send()
+        .map_err(|e| format!("Error fetching file over http {}: {}",
+            url, e.to_string())));
+    if response.status == StatusCode::Ok {
+        let mut data = String::new();
+        try!(response.read_to_string(&mut data)
+            .map_err(|e| format!("Error fetching file over http {}: {}",
+            url, e.to_string())));
+        Ok(data)
+    } else {
+        Err(format!("Error fetching file over http {}: {}",
+            url, response.status))
+    }
+}
+
 fn main() {
 
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use piston_meta::*;
 
     #[test]
@@ -54,5 +82,11 @@ mod tests {
         let _data = load_syntax_data("assets/dependencies/syntax.txt",
             "assets/dependencies/test.txt");
         // json::print(&_data);
+    }
+
+    #[test]
+    fn from_url() {
+        let data = load_text_file_from_url("https://raw.githubusercontent.com/PistonDevelopers/piston/master/src/input/Cargo.toml");
+        assert!(data.is_ok());
     }
 }
