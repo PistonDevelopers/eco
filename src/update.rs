dfd27abcfdff2da8892a808e78a8090f44bec3e7
Sven Nilsen <bvssvni@gmail.com>
2015-08-21 11:18:14 +0200

    Added `Bump, Dependency, Package` to `dependencies`
    See https://github.com/PistonDevelopers/eco/issues/19

--- src/update.rs
+++ src/update.rs
@@ -1,1 +1,33 @@
 //! Generate update information from dependency information.
+
+use std::rc::Rc;
+
+use semver::Version;
+
+/// Stores old and new version.
+pub struct Bump {
+    /// The old version.
+    pub old: Version,
+    /// The new version.
+    pub new: Version,
+}
+
+/// Stores update info for dependency.
+pub struct Dependency {
+    /// The library name.
+    pub name: Rc<String>,
+    /// Bump info.
+    pub bump: Bump,
+}
+
+/// Stores information about a package.
+pub struct Package {
+    /// The package name.
+    pub name: Rc<String>,
+    /// The order to update library.
+    pub order: u32,
+    /// Update information.
+    pub bump: Bump,
+    /// The dependencies.
+    pub dependencies: Vec<Dependency>,
+}
