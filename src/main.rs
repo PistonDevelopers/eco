83956823b7c837913ccacc0b7ef8e7988f41261d
Sven Nilsen <bvssvni@gmail.com>
2015-08-24 13:43:12 +0200

    Added extract data for Piston ecosystem

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() {
     use std::fs::File;
 
     // Load extract info from file.
-    let mut extract_info_file = File::open("assets/extract/test.txt").unwrap();
+    let mut extract_info_file = File::open("assets/extract/piston.txt").unwrap();
     let mut extract_info = String::new();
     extract_info_file.read_to_string(&mut extract_info).unwrap();
 
